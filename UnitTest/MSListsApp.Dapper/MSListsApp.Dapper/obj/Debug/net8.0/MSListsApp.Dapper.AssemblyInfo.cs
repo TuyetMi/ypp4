31e028743a0204948db1bdd142cf412995d9c24f
TuyetMi <luongtuyetmi456@gmail.com>
2025-08-15 19:47:31 +0700

    update 15082025

--- UnitTest/MSListsApp.Dapper/MSListsApp.Dapper/obj/Debug/net8.0/MSListsApp.Dapper.AssemblyInfo.cs
+++ UnitTest/MSListsApp.Dapper/MSListsApp.Dapper/obj/Debug/net8.0/MSListsApp.Dapper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MSListsApp.Dapper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dc2bd476912fb193e6905b42d0653653bba3a8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00cc4af06ceb26fb6f00bfa11e4e44829e1080e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MSListsApp.Dapper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MSListsApp.Dapper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
